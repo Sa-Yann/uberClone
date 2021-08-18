bcf7a7eee2330aa6c1dfb2fda8e2410a7356e56d
Saiyann <knt4xyz@gmail.com>
2021-08-18 16:09:15 +0200

    ✅ Implemant the KeyboardAvoidingView
    NB : style & Behavior  using Platform parameters needs to be included in the KeyboardAvoidingView Component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
@@ -20,6 +20,12 @@ export default function App() {
         {/* <HomeScreen/>  */}
         <NavigationContainer> 
           <SafeAreaProvider > 
+            {/* always give a behavior and style  KeyboardAvoidingView for it to fucntion */}
+            <KeyboardAvoidingView
+              behavior={Platform.OS === "ios" ? "padding" : "height"} 
+              style={{ flex: 1}}
+              // keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+            >
             <Stack.Navigator>
               <Stack.Screen 
                 name="Home Screen" 
@@ -43,17 +49,11 @@ export default function App() {
                 }}
                   />
             </Stack.Navigator>
+            </KeyboardAvoidingView>
           </SafeAreaProvider> 
         </NavigationContainer> 
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
